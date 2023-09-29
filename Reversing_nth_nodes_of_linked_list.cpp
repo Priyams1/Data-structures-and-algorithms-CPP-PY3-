a3815dba7f69ac35010a8bed16d1db7efb995407
Priyam Singh <priyamsingh888@gmail.com>
2023-09-29 07:42:32 +0530

    Add files via upload

---
+++ Reversing_nth_nodes_of_linked_list.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    node *temp = head;
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+// reverse kth node
+
+node *reverseKnodes(node *&head, int k)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+    int count = 0;
+    while (currptr != NULL and count < k)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+    if (nextptr != NULL)
+    {
+        head->next = reverseKnodes(nextptr, k);
+    }
+    return prevptr;
+}
+
+int main()
+{
+    node *head = NULL;
+
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+
+    // insertAtHead(head, 6);
+
+    display(head);
+
+    node *newhead = reverseKnodes(head, 2);
+
+    display(newhead);
+
+    return 0;
+}

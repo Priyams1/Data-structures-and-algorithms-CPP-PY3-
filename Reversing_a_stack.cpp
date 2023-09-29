860e671b6441f53b22972d69002f29d63234905e
Priyam Singh <priyamsingh888@gmail.com>
2023-09-29 07:42:51 +0530

    Add files via upload

---
+++ Reversing_a_stack.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stack>
+
+using namespace std;
+
+void insertAtBottom(stack<int> &st, int element)
+{
+    if (st.empty())
+    {
+        st.push(element);
+        return;
+    }
+    int topelement = st.top();
+    st.pop();
+    insertAtBottom(st, element);
+
+    st.push(topelement);
+}
+void reverse(stack<int> &st)
+{
+    if (st.empty())
+    {
+        return;
+    }
+    int element = st.top();
+    st.pop();
+    reverse(st);
+    insertAtBottom(st, element);
+}
+
+int main()
+{
+    stack<int> st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    reverse(st);
+
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
